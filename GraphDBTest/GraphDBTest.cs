1fb077b520112579a8587340dc5e894a8f76b563
U-Max-PC\Max <Max@Max-PC.(none)>
2012-05-02 00:23:57 +0400

    added union of pair intersection

--- GraphDBTest/GraphDBTest.cs
+++ GraphDBTest/GraphDBTest.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using sones.GraphDB;
 using sones.GraphDB.Request;
 using sones.GraphDB.TypeSystem;
 using sones.GraphDS.PluginManager;
 using sones.GraphDSServer;
+using sones.GraphQL;
 using sones.GraphQL.Result;
 using sones.Library.Commons.Security;
 using sones.Library.PropertyHyperGraph;
 using sones.Library.VersionedPluginManager;
-using sones.GraphQL;
-using System.Net;
 
 namespace GraphDBTest
 {
@@ -33,7 +33,8 @@ namespace GraphDBTest
         /// <summary> Конструктор класса GraphDBTest
         /// Создается база данных graphDB и сервер GraphDSServer
         /// </summary>
-        public GraphDBTest()
+        /// <param name="rest">Определяет, создавать ли rest service или нет</param> 
+        public GraphDBTest(bool rest)
         {
             var graphDB = new SonesGraphDB();
 
@@ -53,13 +54,21 @@ namespace GraphDBTest
 
             edge_list = new List<List< KeyValuePair<long, Int32> >>();
 
-            Dictionary<string, object> RestParameter = new Dictionary<string, object>();
-            RestParameter.Add("IPAddress", IPAddress.Any);
-            RestParameter.Add("Port", 9975);
-            RestParameter.Add("Username", "test");
-            RestParameter.Add("Password", "test");
-            GraphDSServer.StartService("sones.RESTService", RestParameter);
+            if (rest)
+            {
+                Dictionary<string, object> RestParameter = new Dictionary<string, object>();
+                RestParameter.Add("IPAddress", IPAddress.Any);
+                RestParameter.Add("Port", 9975);
+                RestParameter.Add("Username", "test");
+                RestParameter.Add("Password", "test");
+                GraphDSServer.StartService("sones.RESTService", RestParameter);
+            }
         }
+        
+        /// <summary>
+        /// Создает объект базы данных с выключенным rest
+        /// </summary>
+        public GraphDBTest() : this(false){ }
 
         /// <summary> Чистит всю базу данных
         /// 
@@ -74,9 +83,15 @@ namespace GraphDBTest
         /// m - количество ребер;
         /// дальше считываются ребра в список смежности
         /// </summary>
-        private void ReadFile()
+        /// <param name="path">Путь к файлу</param>
+        /// <param name="backward">Делать ли обратные ребра</param>
+        private void ReadFile(string path, bool backward = false)
         {
-            string path = "netData.txt";
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("File does not exist!");
+                return;
+            }
             using (StreamReader sr = new StreamReader(path))
             {
                 vertices = Int32.Parse(sr.ReadLine());
@@ -90,10 +105,10 @@ namespace GraphDBTest
                     int a = Int32.Parse(input_edge[0]);
                     int b = Int32.Parse(input_edge[1]);
                     int c = Int32.Parse(input_edge[2]);
-                    //DateTime date = DateTime.Parse(input_edge[2]);
 
                     edge_list[a].Add( new KeyValuePair<long, Int32>(b, c) );
-                    edge_list[b].Add(new KeyValuePair<long, Int32>(a, c));
+                    if (backward)
+                        edge_list[b].Add(new KeyValuePair<long, Int32>(a, c));
                 }
             }
         }
@@ -101,7 +116,7 @@ namespace GraphDBTest
         /// <summary>
         /// Анализирует результат запроса в базу данных. Показывает ошибки, которые были во время запроса.
         /// </summary>
-        /// <param name="myQueryResult">Результат запроса.</param>
+        /// <param name="myQueryResult">Результат запроса</param>
         private bool CheckResult(IQueryResult myQueryResult)
         {
             if (myQueryResult.Error != null)
@@ -119,6 +134,201 @@ namespace GraphDBTest
 
                 return true;
             }
+        }
+
+        private void Run()
+        {
+            /*var UserVertexPredifinition = new VertexTypePredefinition("User")
+                     .AddProperty(new PropertyPredefinition("Name", "String"))
+                     .AddOutgoingEdge(new OutgoingEdgePredefinition("friends", "User").SetMultiplicityAsMultiEdge());*/
+
+            EdgeTypePredefinition EdgeDate_TypePredefinition = new EdgeTypePredefinition("EdgeDate").
+                AddProperty(new PropertyPredefinition("Time", "Int32"));
+            var UserVertexPredifinition = new VertexTypePredefinition("User")
+                     .AddProperty(new PropertyPredefinition("Name", "String"))
+                     .AddOutgoingEdge(new OutgoingEdgePredefinition("friends", "User").SetMultiplicityAsMultiEdge("EdgeDate"));
+
+
+            var UserEdge = GraphDSServer.CreateEdgeType<IEdgeType>(
+               SecToken,
+               TransactionID,
+               new RequestCreateEdgeType(EdgeDate_TypePredefinition),
+               (Statistics, EdgeType) => EdgeType);
+
+            var UserVertex = GraphDSServer.CreateVertexType<IVertexType>(
+               SecToken,
+               TransactionID,
+               new RequestCreateVertexType(UserVertexPredifinition),
+               (Statistics, VertexTypes) => VertexTypes);
+
+            DateTime dt = DateTime.Now;
+            DateTime full_time = DateTime.Now;
+
+            ReadFile("netData.txt");
+
+            Console.WriteLine("Time of reading: {0}", DateTime.Now - dt);
+            dt = DateTime.Now;
+
+            for (int i = 0; i < vertices; ++i)
+            {
+                EdgePredefinition temp_edge = new EdgePredefinition("friends");
+                for (int j = 0; j < edge_list[i].Count; ++j)
+                    temp_edge.AddEdge(new EdgePredefinition()
+                                        .AddStructuredProperty("Time", edge_list[i][j].Value)
+                                        .AddVertexID("User", edge_list[i][j].Key));
+
+                var ins = GraphDSServer.Insert<IVertex>(
+                         SecToken,
+                         TransactionID,
+                         new RequestInsertVertex("User")
+                             .AddStructuredProperty("Name", "User" + i.ToString())
+                             .AddUnknownProperty("VertexID", i)
+                             .AddEdge(temp_edge),
+                         (Statistics, Result) => Result);
+            }
+
+            Console.WriteLine("Time of inserting: {0}", DateTime.Now - dt);
+            dt = DateTime.Now;
+
+            edge_list.Clear();
+        }
+
+        private void PrintNeighbours(IQueryResult qres)
+        {
+            DateTime dt = DateTime.Now;
+            string path = "netData_output.txt";
+            if (!File.Exists(path))
+                File.Create(path);
+            StreamWriter sw = new StreamWriter(path);
+            foreach (var vert in qres.Vertices)
+            {
+                sw.Write("Neighbours of {0}: ", vert.GetPropertyAsString("Name"));
+                var neighbours_list = vert.GetProperty<List<VertexView>>("friends");
+                if (neighbours_list != null)
+                {
+                    sw.WriteLine("Count: {0}", neighbours_list.Count);
+                    foreach (var neighbour in neighbours_list)
+                        sw.Write("{0} ", neighbour.GetPropertyAsString("VertexID"));
+                }
+                else
+                    sw.Write("None");
+                sw.WriteLine();
+            }
+            sw.Close();
+
+            Console.WriteLine("Time of printing to file: {0}", DateTime.Now - dt);
+        }
+
+        private void Neighbours()
+        {
+            DateTime dt = DateTime.Now;
+
+            var qres = GraphDSServer.Query(SecToken, TransactionID,
+                    "FROM User SELECT Name, friends.neighbours(5)",
+                    SonesGQLConstants.GQL);
+
+            CheckResult(qres);
+
+            Console.WriteLine("Time of finding neighbours: {0}", DateTime.Now - dt);
+            PrintNeighbours(qres);
+
+            //dt = DateTime.Now;
+        }
+
+        private void UnionOfPairIntersections(int depth)
+        {
+            DateTime dt = DateTime.Now;
+            var qres = GraphDSServer.Query(SecToken, TransactionID,
+                    "FROM User SELECT Name, friends.neighbours(" + depth.ToString() + ")",
+                    SonesGQLConstants.GQL);
+            CheckResult(qres);
+
+            var vertices = qres.Vertices;
+
+            string path = "UnionofPairs_output.txt";
+            StreamWriter sw = new StreamWriter(path);
+            HashSet<int> answer = new HashSet<int>();
+            foreach (var vertex1 in vertices)
+            {
+                foreach (var vertex2 in vertices)
+                {
+
+                    Dictionary<int, int> intersection = new Dictionary<int, int>();
+                    
+                    var neighbours_list = vertex1.GetProperty<List<VertexView>>("friends");
+                    if (neighbours_list != null)
+                    {
+                        foreach (var neighbour in neighbours_list)
+                        {
+                            int vert_id = neighbour.GetProperty<int>("VertexID");
+                            if (intersection.ContainsKey(vert_id))
+                                intersection[vert_id]++;
+                            else
+                                intersection.Add(vert_id, 1);
+                        }
+                    }
+                    neighbours_list = vertex2.GetProperty<List<VertexView>>("friends");
+                    if (neighbours_list != null)
+                    {
+                        foreach (var neighbour in neighbours_list)
+                        {
+                            int vert_id = neighbour.GetProperty<int>("VertexID");
+                            if (intersection.ContainsKey(vert_id))
+                                intersection[vert_id]++;
+                            else
+                                intersection.Add(vert_id, 1);
+                        }
+                    }
+
+                    
+                    if (!File.Exists(path))
+                        File.Create(path);
+                    
+                    Console.WriteLine("Intersection of " + vertex1.GetPropertyAsString("Name") + " " + vertex2.GetPropertyAsString("Name"));
+                    
+                    foreach (var pair in intersection)
+                    {
+                        if (pair.Value >= 2)
+                            if (!answer.Contains(pair.Key))
+                                answer.Add(pair.Key);
+                    }
+                    
+                }
+
+            }
+
+            sw.WriteLine("Count: " + answer.Count);
+            foreach (int id in answer)
+                sw.Write(id.ToString() + " ");
+            sw.Close();
+            Console.WriteLine("Time of finding and printing intersection: {0}", DateTime.Now - dt);
+        }
+
+        static void Main(string[] args)
+        {
+            GraphDBTest DB = new GraphDBTest();
+            Console.WriteLine("DB was started successfull!");
+            DB.Run();
+
+            //DB.Neighbours();
+            DB.UnionOfPairIntersections(2);
+        }
+
+        /// <summary>
+        /// Trash //////////////////////////////////////////////////////////////////////////
+        /// </summary>
+
+        private void FastImport()
+        {
+            //ClearGraphDB();
+            //var cr = GraphDSServer.Query(SecToken, TransactionID,
+            //                    "CREATE VERTEX TYPE User ATTRIBUTES (String Name, Int64 Age, Set<User> Friends)",
+            //                    SonesGQLConstants.GQL);
+
+            var imp = GraphDSServer.Query(SecToken, TransactionID,
+                                "IMPORT FROM 'file:\\\\10k_import.xml' FORMAT FastImport",
+                                SonesGQLConstants.GQL);
+
         }
 
         private void PrintWays(IVertexView current_vertex, int lev = 0)
@@ -151,169 +361,6 @@ namespace GraphDBTest
                             user.GetPropertyAsString(attrName.ID),
                             edge.GetTargetVertex().GetPropertyAsString(attrName.ID),
                             edge.GetPropertyAsString(attrFriendsDate.ID));
-        }
-
-        private void Run()
-        {
-            /*var UserVertexPredifinition = new VertexTypePredefinition("User")
-                     .AddProperty(new PropertyPredefinition("Name", "String"))
-                     .AddOutgoingEdge(new OutgoingEdgePredefinition("friends", "User").SetMultiplicityAsMultiEdge());*/
-
-            EdgeTypePredefinition EdgeDate_TypePredefinition = new EdgeTypePredefinition("EdgeDate").
-                AddProperty(new PropertyPredefinition("Time", "Int32"));
-            var UserVertexPredifinition = new VertexTypePredefinition("User")
-                     .AddProperty(new PropertyPredefinition("Name", "String"))
-                     .AddOutgoingEdge(new OutgoingEdgePredefinition("friends", "User").SetMultiplicityAsMultiEdge("EdgeDate"));
-
-
-            var UserEdge = GraphDSServer.CreateEdgeType<IEdgeType>(
-               SecToken,
-               TransactionID,
-               new RequestCreateEdgeType(EdgeDate_TypePredefinition),
-               (Statistics, EdgeType) => EdgeType);
-
-            var UserVertex = GraphDSServer.CreateVertexType<IVertexType>(
-               SecToken,
-               TransactionID,
-               new RequestCreateVertexType(UserVertexPredifinition),
-               (Statistics, VertexTypes) => VertexTypes);
-
-            DateTime dt = DateTime.Now;
-            DateTime full_time = DateTime.Now;
-
-            ReadFile();
-
-            Console.WriteLine("Time of reading: {0}", DateTime.Now - dt);
-            dt = DateTime.Now;
-
-            for (int i = 0; i < vertices; ++i)
-            {
-                EdgePredefinition temp_edge = new EdgePredefinition("friends");
-                for (int j = 0; j < edge_list[i].Count; ++j)
-                    temp_edge.AddEdge(new EdgePredefinition()
-                                        .AddStructuredProperty("Time", edge_list[i][j].Value)
-                                        .AddVertexID("User", edge_list[i][j].Key));
-
-                var ins = GraphDSServer.Insert<IVertex>(
-                         SecToken,
-                         TransactionID,
-                         new RequestInsertVertex("User")
-                             .AddStructuredProperty("Name", "User" + i.ToString())
-                             .AddUnknownProperty("VertexID", i)
-                             .AddEdge(temp_edge),
-                         (Statistics, Result) => Result);
-            }
-
-            Console.WriteLine("Time of inserting: {0}", DateTime.Now - dt);
-            dt = DateTime.Now;
-
-            edge_list.Clear();
-            
-            //PrintAllVerticesInBase(UserVertex);
-
-            //Console.WriteLine("Time of selecting: {0}", DateTime.Now - dt);
-            //Console.WriteLine("Full time: {0}", DateTime.Now - full_time);
-
-            //var qres_dij = GraphDSServer.Query(SecToken, TransactionID,
-            //                    "FROM User U SELECT U.Name, U.friends.Dijkstra(U.Name = 'User4', 5, 'EdgeDate') AS 'path' WHERE U.Name = 'User0'",
-            //                    SonesGQLConstants.GQL);
-
-            //var qres = GraphDSServer.Query(SecToken, TransactionID,
-            //                                "FROM User U SELECT U.Name, U.friends.PATH(U.Name = 'User4', 5, 5, true, false) AS 'path' WHERE U.Name = 'User0' OR U.Name = 'User1'",
-            //                                SonesGQLConstants.GQL);
-
-            //Console.WriteLine("Time of BFS: {0}", DateTime.Now - dt);
-            //dt = DateTime.Now;
-
-            //CheckResult(qres);
-
-            //var first_vert = qres.Vertices.FirstOrDefault();
-
-            //foreach (var vert in qres.Vertices)
-            //    PrintWays(vert);
-
-            //Console.WriteLine("Time of printing: {0}", DateTime.Now - dt);
-            //Console.WriteLine("Full time: {0}", DateTime.Now - full_time);
-        }
-
-        private void PrintNeighbours(IQueryResult qres)
-        {
-            DateTime dt = DateTime.Now;
-
-            StreamWriter sw = new StreamWriter("netData_output.txt");
-            foreach (var vert in qres.Vertices)
-            {
-                sw.Write("Neighbours of {0}: ", vert.GetPropertyAsString("Name"));
-                var neighbours_list = vert.GetProperty<List<VertexView>>("friends");
-                if (neighbours_list != null)
-                {
-                    sw.WriteLine("Count: {0}", neighbours_list.Count);
-                    foreach (var neighbour in neighbours_list)
-                        sw.Write("{0} ", neighbour.GetPropertyAsString("VertexID"));
-                }
-                else
-                    sw.Write("None");
-                sw.WriteLine();
-            }
-            sw.Close();
-
-            Console.WriteLine("Time of printing to file: {0}", DateTime.Now - dt);
-        }
-
-        private void Neighbours()
-        {
-            DateTime dt = DateTime.Now;
-            //var qres = GraphDSServer.Query(SecToken, TransactionID,
-            //                                "FROM User SELECT Name, friends.neighbours(2)",
-            //                                SonesGQLConstants.GQL);
-
-            //Console.WriteLine("Time of finding neighbours: {0}", DateTime.Now - dt);
-            //PrintNeighbours(qres);
-
-            //dt = DateTime.Now;
-
-            //var qres = GraphDSServer.Query(SecToken, TransactionID,
-            //                    "FROM User SELECT Name, friends.neighbours(5)",
-            //                    SonesGQLConstants.GQL);
-
-            //Console.WriteLine("Time of finding neighbours: {0}", DateTime.Now - dt);
-            //PrintNeighbours(qres);
-
-            //dt = DateTime.Now;
-
-            var qres = GraphDSServer.Query(SecToken, TransactionID,
-                    "FROM User SELECT Name, friends.neighbours(5)",
-                    SonesGQLConstants.GQL);
-
-            CheckResult(qres);
-
-            Console.WriteLine("Time of finding neighbours: {0}", DateTime.Now - dt);
-            PrintNeighbours(qres);
-
-            //dt = DateTime.Now;
-        }
-
-        private void FastImport()
-        {
-            //ClearGraphDB();
-            //var cr = GraphDSServer.Query(SecToken, TransactionID,
-            //                    "CREATE VERTEX TYPE User ATTRIBUTES (String Name, Int64 Age, Set<User> Friends)",
-            //                    SonesGQLConstants.GQL);
-
-            var imp = GraphDSServer.Query(SecToken, TransactionID,
-                                "IMPORT FROM 'file:\\\\10k_import.xml' FORMAT FastImport",
-                                SonesGQLConstants.GQL);
-            
-        }
-
-        static void Main(string[] args)
-        {
-            GraphDBTest DB = new GraphDBTest();
-            Console.WriteLine("DB was started successfull!");
-            DB.Run();
-            //DB.FastImport()
-
-            DB.Neighbours();
         }
     }
 }
